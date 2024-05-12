7d5d5ace42a59bd0f56dd59e0c11d74656296b3a
CowNow <133740174+CowNowK@users.noreply.github.com>
2024-05-12 09:42:04 +0800

    Update app.js

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const totp = require('./totp.js');
+const totp = require('../totp.min.js');
 
 app.get('/api/otp', (req, res) => {
     res.send(totp.getUTCTime());
